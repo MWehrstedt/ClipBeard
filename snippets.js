1e6ca7d32446e7166835a5891bd3aa7029518a54
Marcel Wehrstedt <marcel.wehrstedt@outlook.com>
2017-11-12 18:50:14 +0100

    Added remove button

--- snippets.js
+++ snippets.js
@@ -6,11 +6,13 @@ var count = 0;
 /* add event listeners */
 addBtn.addEventListener('click', addSnippet);
 clearBtn.addEventListener('click', clearSnippets);
-document.addEventListener('click', function (e) {
+snippetList.addEventListener('click', function (e) {
 
     // check if list item is clicked, do nothing otherwise
-    if (e.target && e.target.nodeName == 'LI') {
+    if (e.target && e.target.nodeName == 'DIV') {
         // get text to copy
+        console.log(e.target);
+
         var copyBody = e.target.innerHTML;
 
         // create a temporary textbox to copy text from
@@ -39,7 +41,7 @@ function initialize() {
     // load storage items to items list
     var storageItems = browser.storage.sync.get(function (items) {
         for (var element in items)
-            addSnippetToList(items[element]);
+            addSnippetToList(items[element], element);
     });
 }
 
@@ -59,14 +61,59 @@ function saveSnippet(content) {
     browser.storage.sync.set({ [snippetHeader]: content });
 
     // Add snippet to List
-    addSnippetToList(snippetText);
+    addSnippetToList(snippetText, snippetHeader);
 }
 
-function addSnippetToList(text) {
+function addSnippetToList(text, nid) {
     // Create new list item and add it to the list
     var list = document.getElementById('snippetList');
     var sNode = document.createElement('li');
-    sNode.innerText = text;
+
+    // parent node
+    var sItem = document.createElement('div');
+    var sItemAttr = document.createAttribute('class');
+    sItemAttr.value = 'itemArea';
+    sItem.setAttributeNode(sItemAttr);
+
+    // displayed content area
+    var sContentArea = document.createElement('div');
+    var sContAttr = document.createAttribute('class');
+    sContAttr.value = 'itemContentArea';
+    sContentArea.setAttributeNode(sContAttr);
+    sContentArea.innerText = text;
+
+    // edit button area
+    var sEditArea = document.createElement('div');
+    var sEditAttr = document.createAttribute('class');
+    sEditAttr.value = 'itemEditArea';
+    sEditArea.setAttributeNode(sEditAttr);
+    
+    // create erase button
+    var sEditButton = document.createElement('button');
+    var sEditButtonAttr = document.createAttribute('class');
+    sEditButtonAttr.value = 'itemEditButton';
+    sEditButton.setAttributeNode(sEditButtonAttr);
+    var sEditButtonIdAttr = document.createAttribute('id');
+    sEditButtonIdAttr.value = nid;
+    sEditButton.setAttributeNode(sEditButtonIdAttr);
+    sEditButton.addEventListener('click', function() {
+        browser.storage.sync.remove(nid);
+
+        // redraw list
+        var list = document.getElementById('snippetList');
+        while (list.firstChild)
+            list.removeChild(list.firstChild);
+
+        initialize();
+        console.log('Removed entry ' + this.id );
+    }, false);
+
+    sEditArea.appendChild(sEditButton);
+    
+    // add created nodes to parent node
+    sItem.appendChild(sContentArea);
+    sItem.appendChild(sEditArea);
+    sNode.appendChild(sItem);
     list.appendChild(sNode);
 
     // Clear textbox
