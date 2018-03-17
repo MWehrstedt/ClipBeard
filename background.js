daef189b1cc3aeae5f71a8707fe8efff5c7307b5
Marcel Wehrstedt <marcel.wehrstedt@outlook.com>
2018-03-17 18:09:06 +0100

    Added check for saved snippets, tweaked badge colors

--- background.js
+++ background.js
@@ -19,11 +19,28 @@ browser.contextMenus.onClicked.addListener(function (info, tab) {
     if (info.menuItemId === 'clipbeardSelectionMenuItem') {
         var snippetText = info.selectionText;
         var snippetHeader = new Date().toUTCString();
-        browser.storage.sync.set({ [snippetHeader]: snippetText });
-        browser.browserAction.setBadgeText({ text: '+' });
+
+        // Iterate through every storage entry and show badge, if snippet is already saved
+        var storageItems = browser.storage.sync.get(function (items) {
+            for (var element in items) {
+                if (items[element] === snippetText) {
+
+                    // Show a star character (unicode u+2605) on an orange background
+                    browser.browserAction.setBadgeText({ text: String.fromCharCode(9733) });
+                    browser.browserAction.setBadgeBackgroundColor({color: "#FFA500"});
+                    return;
+                }
+            }
+
+            // Show a plus on a green background
+            browser.storage.sync.set({ [snippetHeader]: snippetText });
+            browser.browserAction.setBadgeBackgroundColor({color: "#00CC00"});
+            browser.browserAction.setBadgeText({ text: '+' });
+        });
+
 
         setTimeout(function () {
             browser.browserAction.setBadgeText({ text: "" });
-        }, 600000);
+        }, 300000);
     }
 });
